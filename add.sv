77c39d30fc18423d696d31958b312239ef0fc88c
VLSI_Vandu <ashishsharma521998@gmail.com>
2026-05-02 20:23:53 +0000

    registering the add

--- add.sv
+++ add.sv
@@ -1,13 +1,21 @@
-module add (input  wire din_a,
+module add (
+	    
+	    input  wire clk,
+	    input  wire rst,
+	    input  wire din_a,
 	    input  wire din_b,
 	    input  wire din_cin,
 	    output reg dout_sum,
 	    output reg dout_carry
 	    );
+       reg dout_sum_reg;
+       reg dout_carry_reg;
 
-	 always@(*) begin
-                  {dout_carry,dout_sum} = (din_a+din_b+din_cin);
-         end
+	      always@(posedge clk) begin
+                     if(rst) 
+                              {dout_carry,dout_sum} = 2'b00;
+                     else 
+                              {dout_carry,dout_sum} = (din_a+din_b+din_cin);
 endmodule
 
 ######### Test pattern
