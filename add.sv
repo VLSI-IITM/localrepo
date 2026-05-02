ced14c40ed43a7e0679ca8699dae75d7aa5967e9
VLSI_Vandu <ashishsharma521998@gmail.com>
2026-05-02 19:55:36 +0000

    added to feauture1

--- add.sv
+++ add.sv
@@ -9,3 +9,14 @@ module add (input  wire din_a,
                   {dout_carry,dout_sum} = (din_a+din_b+din_cin);
          end
 endmodule
+
+######### Test pattern
+I/P O
+000 00
+001 01
+010 01
+011 10
+100 01
+101 10
+110 10
+111 11
